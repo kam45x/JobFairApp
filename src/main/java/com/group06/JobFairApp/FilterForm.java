6cc3999d0574aa2ed7814631d124ac201994164f
kcisek <kamil.cisek03@gmail.com>
2024-05-20 22:24:08 +0200

    Filters backend added

---
+++ src/main/java/com/group06/JobFairApp/FilterForm.java
@@ -0,0 +1,11 @@
+package com.group06.JobFairApp;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class FilterForm {
+    private List<String> selectedFilters;
+}

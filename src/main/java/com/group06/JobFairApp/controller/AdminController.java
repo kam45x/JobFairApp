b05a5e3677cbcbc324a51242aba97fd5f3f8f0f1
psienkie <01178247@pw.edu.pl>
2024-05-27 15:45:45 +0200

    Added companies list to the adminView. Implemented removing companies. Started implementing companies editing option.

--- src/main/java/com/group06/JobFairApp/controller/AdminController.java
+++ src/main/java/com/group06/JobFairApp/controller/AdminController.java
@@ -1,15 +1,46 @@
 package com.group06.JobFairApp.controller;
 
-
+import com.group06.JobFairApp.model.Company;
+import com.group06.JobFairApp.repository.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/adminView")
 public class AdminController {
-    @GetMapping("/adminView")
-    public String adminView() {
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @GetMapping
+    public String adminView(Model model) {
+        model.addAttribute("companies", companyRepository.findAll());
         return "adminview/adminView";
     }
 
+    @GetMapping("/editCompany/{id}")
+    public String editCompanyForm(@PathVariable("id") Long id, Model model) {
+        Company company = companyRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid company Id:" + id));
+        model.addAttribute("company", company);
+        return "adminview/editCompany";
+    }
+
+    @PostMapping("/saveCompany")
+    public String saveCompany(@ModelAttribute("company") Company company, BindingResult result) {
+        if (result.hasErrors()) {
+            return "adminview/editCompany";
+        }
+        companyRepository.save(company);
+        return "redirect:/adminView";
+    }
+
+    @GetMapping("/deleteCompany/{id}")
+    public String deleteCompany(@PathVariable("id") Long id) {
+        Company company = companyRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid company Id:" + id));
+        companyRepository.delete(company);
+        return "redirect:/adminView";
+    }
 }
--- src/main/java/com/group06/JobFairApp/repository/CompanyRepository.java
+++ src/main/java/com/group06/JobFairApp/repository/CompanyRepository.java
@@ -4,9 +4,12 @@ import com.group06.JobFairApp.model.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
     Company findByBoothNumber(int boothNumber);
+    Optional<Company> findById(Long id);
     Company findByName(String name);
     Company findByWebsiteUrl(String websiteUrl);
 }
